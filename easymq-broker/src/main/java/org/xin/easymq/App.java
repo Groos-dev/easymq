641611abc67577b4d661afb40fa9a85ed7828aa6
曾薪 <zengxin2@yonyou.com>
2023-05-09 09:41:44 +0800

    初始化项目

---
+++ easymq-broker/src/main/java/org/xin/easymq/App.java
@@ -0,0 +1,13 @@
+package org.xin.easymq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
